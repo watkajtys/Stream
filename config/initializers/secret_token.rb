b5d9600615210a26812f853015927c4d48704973
Matt Watkajtys <watkajtys@gmail.com>
2013-05-01 19:49:04 -0400

    Initial commit.
    Skipped active record and generated mongodb.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stream::Application.config.secret_token = 'ee258a45791e2bb03dbf0469ab7cc0c3a84d26fb6c77f8ecafc6a9d82fee89806431f94c619ec0e3e77960c71a11fb26eea48468c36bc11a8b629620343c42e3'
