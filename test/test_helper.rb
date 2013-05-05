c48cefc5267615ce113923f1c26a9b8888f8c52c
Matt Watkajtys <watkajtys@gmail.com>
2013-05-05 19:22:03 -0400

    Added collection dropping before tests run.
    Apparently test data is persistent in a mongoDB database. This needs to be dropped manually for a test to run correctly.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,5 +3,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Add more helper methods to be used by all tests here...
+  # Drop all collections after each test case.
+  Mongoid.default_session.collections.select {|c| c.name !~ /system/ }.each(&:drop)
+
 end
