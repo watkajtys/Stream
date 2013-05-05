d718e1653f894855ef5cbdf05d1a7cd74a31377f
Matt Watkajtys <watkajtys@gmail.com>
2013-05-05 18:24:09 -0400

    Added routing for api/vi/images

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Stream::Application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :images
+    end
+  end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
