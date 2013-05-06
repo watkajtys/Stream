51598bdf45847cd3a77dc14ac0554e0064cfe337
Matt Watkajtys <watkajtys@gmail.com>
2013-05-05 20:00:16 -0400

    Added unit test to ensure URL in filled in

--- test/unit/image_test.rb
+++ test/unit/image_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class ImageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "url is required" do
+      image = Image.new
+      assert_false(image.save)
+   end
 end
